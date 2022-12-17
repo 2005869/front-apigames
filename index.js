ed9f2bc38444965e2ee33f8866152ec8c281cb58
2005869 <2005869@aluno.univesp.br>
2022-12-16 23:31:22 -0300

    change li to table add bootstrap and others

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
+const PORT = 8080;
 const bodyParser = require('body-parser');
+const UserController = require('./users/UsersController');
+const session = require('express-session');
+const usersAuth = require('./middlewares/usersauth');
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// session
+app.use(session({
+    secret: "gyagdfygaysdgafsdaa",
+    cookie: {maxAge: 30000}
+}));
+
+
+
+app.use('/', UserController);
+
+//Rotes
 app.get('', (req, res) => {
     res.render('index');
 });
 
-app.listen(8080, () => {
+
+app.listen(PORT, () => {
     console.log('front-apigames start');
 });
