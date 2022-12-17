78b3b772b06311d344dbb71a7dc21d67e300b8ce
2005869 <2005869@aluno.univesp.br>
2022-12-16 23:30:10 -0300

    add middlewares

---
+++ middlewares/usersauth.js
@@ -0,0 +1,10 @@
+function usersAuth(req, res, next){
+    if (req.session.user != undefined){
+        next();
+    }
+    else{
+        res.redirect('/login');
+    }
+}
+
+module.exports = usersAuth;
